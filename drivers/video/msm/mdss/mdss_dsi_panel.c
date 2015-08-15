e0a8de6b8d29e5ed39e7adf07d4dc0741098371c
placiano <placiano80@gmail.com>
2015-08-15 13:42:10 +0200

    Display hooks for Power Suspend
    Signed-off-by: placiano <placiano80@gmail.com>
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi_panel.c
    
    Change-Id: I457a940f9733811ba217de6b61f0e589ff47e22d

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -21,6 +21,9 @@
 #include <linux/leds.h>
 #include <linux/qpnp/pwm.h>
 #include <linux/err.h>
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
 
 #include "mdss_dsi.h"
 
@@ -772,6 +775,9 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_BLANK;
 	pr_debug("%s:-\n", __func__);
+#ifdef CONFIG_POWERSUSPEND
+    set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 	return 0;
 }
 
